b12ea521b1216332327996a6449eec67f672f635
gedvrk <gedvrk@gmail.com>
2021-06-08 03:15:32 +0300

    Add a basic ICommand implementation

--- MindAllot/Viewmodels/MainViewModel.cs
+++ MindAllot/Viewmodels/MainViewModel.cs
@@ -1,6 +1,22 @@
-﻿namespace MindAllot.ViewModels
+﻿using MindAllot.Commands;
+using MindAllot.Views.Popups;
+
+namespace MindAllot.ViewModels
 {
     public class MainViewModel
     {
+        public RelayCommand NewIdeaCommand { get; set; }
+
+        public MainViewModel()
+        {
+            NewIdeaCommand = new RelayCommand(o => { OpenNewIdeaDialog(); });
+        }
+
+        private void OpenNewIdeaDialog()
+        {
+            AddNewTaskDialog dialog = new AddNewTaskDialog();
+
+            _ = dialog.ShowDialog();
+        }
     }
 }
